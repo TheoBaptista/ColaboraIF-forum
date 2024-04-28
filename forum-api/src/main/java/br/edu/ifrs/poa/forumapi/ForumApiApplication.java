c54f728ff3c2e9ae1d4463b5f9150bf6b845bc8e
Theo Baptista <theoalfonso78@gmail.com>
2024-04-28 18:51:14 -0300

    feat: project bootstrap

---
+++ forum-api/src/main/java/br/edu/ifrs/poa/forumapi/ForumApiApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifrs.poa.forumapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForumApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForumApiApplication.class, args);
+	}
+
+}
